e2d8005e5413427f9d4978f1a8cd7f4ffdc30cea
Augustin Trancart <augustin.trancart@oslandia.com>
2020-12-01 17:55:34 +0100

    Add cpp version for day1

---
+++ year_2020/day1/part1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+int main() {
+  vector<int> v;
+
+  // load file
+  ifstream my_file("./input");
+  if (!my_file) {
+    cerr << "No such file..." << endl;
+    return 1;
+  }
+
+  // read file
+  auto start = chrono::high_resolution_clock::now();
+  int number;
+  while (my_file >> number) {
+    v.push_back(number);
+  }
+
+  // iteration
+  for (auto i = begin(v); i != end(v); i++) {
+    for (auto j = i; j != end(v); j++) {
+      if (*i + *j == 2020) {
+        cout << "Result: " << *i * *j << endl;
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        cout << "Exec Time: " << duration.count() << endl;
+        my_file.close();
+        return 0;
+      }
+
+    }
+  }
+
+  return 0;
+}
