ccda0e2313f93546afd856619e7571de5667de05
Augustin Trancart <augustin.trancart@oslandia.com>
2020-12-03 10:14:45 +0100

    Day3 part2: clean, better naming, doc

--- year_2020/day3/load.sql
+++ year_2020/day3/load.sql
@@ -1,10 +1,11 @@
 drop table if exists day3_input;
 drop table if exists day3;
 
+-- also generating line_numbers here, as SGBD generally doesn't guarantee row orders in select queries
+-- PostgreSQL DOES guarantee the order in copy queries though, that's why it works
 create temporary table day3_input(line_number serial, iso text);
 
 \copy day3_input(iso) from './input';
-
 
 create table day3 as
     select line_number, string_to_array(iso, NULL) as iso from day3_input;
@@ -12,4 +13,4 @@ create table day3 as
 -- OOOHH NO, postgresqsl does not have multiply aggregate function /o\
 -- sad!
 -- well, no big deal in fact
-CREATE AGGREGATE mult(bigint) ( SFUNC = int8mul, STYPE=bigint );
+create or replace aggregate mult(bigint) ( SFUNC = int8mul, STYPE=bigint );
--- year_2020/day3/part2.sql
+++ year_2020/day3/part2.sql
@@ -1,12 +1,16 @@
--- easy peasy lemon squeezy (2)
+-- easy peasy lemon squeezy (2) (but a bit less than part 1, let's be honest)
+
 -- I pity the reader (me in approx 1 week), let's do a CTE this time
--- hardcoding the different paths is against all my principles
 \set bound 9
-with path as (
-  select s % (:bound - 1) as right, 1+ (s / :bound) as down from generate_series(1, :bound, 2) s
+-- first generate the possible "move", down and right
+-- hardcoding them is against all my principles
+with move as (
+  select s % (:bound - 1) as right, 1+ (s / :bound) as down
+  from generate_series(1, :bound, 2) s
 )
+-- mult doesn't exist in postgres, it is created in load.sql
 select mult("Bruise count") as "Answer!!!"
-from path
+from move
 -- lateral FTW
 join lateral (
   select count(*) as "Bruise count"
@@ -14,11 +18,11 @@ join lateral (
       -- a bit of complexity here because arrays are 1-based in postgresql
       -- that's a case where I'd definitely have preferred 0-based indexing
       -- but maybe it's because I'm more used to it?
-      select iso[1 + ((line_number - 1) * path.right) % (cardinality(iso))] = '#' "Ouch!!"-- ouch !
+      select iso[1 + ((line_number - 1) * move.right) % (cardinality(iso))] = '#' "Ouch!!"-- ouch !
       from day3
       -- this only select 1 lines every "down" line
-      where line_number % path.down = 0
+      where line_number % move.down = 0
   ) as ouch_list
   -- count only when "ouch!"
   where "Ouch!!"
-) as ouch_count_path on true;
+) as ouch_count_per_move on true;
