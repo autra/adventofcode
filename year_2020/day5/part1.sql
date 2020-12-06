4d58372949e200d6c7f09a8d63146f94fee1e47d
Augustin Trancart <augustin.trancart@oslandia.com>
2020-12-06 14:48:36 +0100

    Remove comments not in the correct place

--- year_2020/day5/part1.sql
+++ year_2020/day5/part1.sql
@@ -1,7 +1,3 @@
--- Alternative solution I really
--- like https://github.com/xocolatl/advent-of-code/blob/master/2020/dec05.sql
--- part2 is really better there :-)
-
 -- FLRB are actually a binary number representing up to 2^10
 -- convert to binary then integer
 select max(translate(seat, 'FLBR', '0011')::bit(10)::integer) "Highest seat id" from day5
